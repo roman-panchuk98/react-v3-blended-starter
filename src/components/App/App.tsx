81a1735b519a2d533eb9acccead0108ad8fdf58e
Roman Panchuk <panchuk.roman98@gmail.com>
2025-09-21 15:58:53 +0200

    Fixed

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -20,7 +20,6 @@ export default function App() {
 
   const handleChange = useDebouncedCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchWord(event.target.value);
-
     setCurrentPage(1);
   }, 300);
 
